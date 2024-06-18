79b9429e93b346903d1d06ee540e6188cadef7ee
Rcaradec <remi.caradec@matou-technologies.fr>
2024-06-18 11:49:41 +0200

    [Purrfect-UI] Border - clean

--- src/guidelines/theme/style.stories.tsx
+++ src/guidelines/theme/style.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 import styled from "styled-components";
-import { HexColor, heading1, spacing, theme } from "./index";
+import { getContrastYIQ, heading1, spacing, theme } from "./index";
 
 const meta: Meta = {};
 
@@ -24,28 +24,6 @@ const Rectangle = styled.div<{
   font-size: 0%.875rem;
   background-color: ${({ bgColor }) => bgColor};
 `;
-
-function getContrastYIQ(hexColor: string | HexColor) {
-  let colorString: string | undefined;
-  if (typeof hexColor === "string") {
-    colorString = hexColor;
-  } else {
-    colorString = hexColor.light || hexColor.dark || hexColor.white;
-
-    if (!colorString) {
-      console.error("getContrastYIQ: No valid color property found in object.");
-      throw new Error("getContrastYIQ: colorString is undefined.");
-    }
-  }
-
-  const r = parseInt(colorString.substring(1, 3), 16);
-  const g = parseInt(colorString.substring(3, 5), 16);
-  const b = parseInt(colorString.substring(5, 7), 16);
-
-  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
-
-  return yiq >= 128 ? "dark" : "light";
-}
 
 const Title = styled.h1`
   ${heading1};
