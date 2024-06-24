88395475aba35baaa857344a92d6f7bd74afa6fe
Remi Caradec <72319136+Rcaradec@users.noreply.github.com>
2024-06-24 14:20:45 +0200

    [Form] Switch (#16)
    * [Form] Switch
    
    * [Form] Switch - PR Review / Checkbox refacto / Slider StyledLabel styling
    
    * [Form] Switch - css mod -> data-disabled for handling isDisabled prop from Aria React Component

--- src/form/Checkbox/Checkbox.tsx
+++ src/form/Checkbox/Checkbox.tsx
@@ -2,71 +2,74 @@ import React from "react";
 import {
   Checkbox as AriaCheckbox,
   CheckboxProps as AriaCheckboxProps,
+  Label,
 } from "react-aria-components";
 import styled from "styled-components";
+import { sizing, theme } from "../../guidelines/theme";
 
-const StyledCheckbox = styled(AriaCheckbox)`
+const StyledCheckboxContainer = styled(AriaCheckbox)`
   display: flex;
   align-items: center;
-  gap: 0.571rem;
+  gap: ${sizing(9)};
   font-size: 1.143rem;
-  color: #111111;
+  color: ${theme.color.text.dark};
   forced-color-adjust: none;
+`;
 
-  .checkbox {
-    width: 1.143rem;
-    height: 1.143rem;
-    border: 2px solid #d2d2d2;
-    border-radius: 4px;
-    transition: all 200ms;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+const StyledCheckbox = styled.div`
+  width: ${sizing(18)};
+  height: ${sizing(18)};
+  border: 2px solid ${theme.color.gray600};
+  border-radius: ${sizing(4)};
+  transition: all 200ms;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  [data-pressed] & {
+    border-color: ${theme.color.gray200};
   }
 
-  svg {
-    width: 1rem;
-    height: 1rem;
-    fill: none;
-    stroke: #f0f0f0;
-    stroke-width: 3px;
-    stroke-dasharray: 22px;
-    stroke-dashoffset: 66;
-    transition: all 200ms;
-  }
-
-  &[data-pressed] .checkbox {
-    border-color: #b5b5b5;
-  }
-
-  &[data-focus-visible] .checkbox {
-    outline: 2px solid #b9b4eb;
+  [data-focus-visible] & {
+    outline: 2px solid ${theme.color.primary300};
     outline-offset: 2px;
   }
 
-  &[data-selected],
-  &[data-indeterminate] {
-    .checkbox {
-      border-color: #3b31b1;
-      background: #3b31b1;
-    }
+  [data-selected] &,
+  [data-indeterminate] & {
+    border-color: ${theme.color.primary};
+    background: ${theme.color.primary};
 
-    &[data-pressed] .checkbox {
-      border-color: #5b51d1;
-      background: #5b51d1;
-    }
-
-    svg {
-      stroke-dashoffset: 44;
+    [data-pressed] & {
+      border-color: ${theme.color.primary};
+      background: ${theme.color.primary};
     }
   }
+`;
 
-  &[data-indeterminate] {
-    svg {
-      stroke: none;
-      fill: #f0f0f0;
-    }
+const StyledSvg = styled.svg`
+  width: 1rem;
+  height: 1rem;
+  fill: none;
+  stroke: ${theme.color.gray100};
+  stroke-width: 3px;
+  stroke-dasharray: 22px;
+  stroke-dashoffset: 66;
+  transition: all 200ms;
+
+  [data-selected] &,
+  [data-indeterminate] & {
+    stroke-dashoffset: 44;
   }
+
+  [data-indeterminate] & {
+    stroke: none;
+    fill: ${theme.color.gray100};
+  }
+`;
+
+const StyledLabel = styled(Label)`
+  color: ${theme.color.text.dark};
 `;
 
 type CheckboxProps = {
@@ -79,14 +82,14 @@ type CheckboxProps = {
 
 const Checkbox = (props: CheckboxProps) => {
   return (
-    <StyledCheckbox {...props}>
-      <div className="checkbox">
-        <svg viewBox="0 0 18 18" aria-hidden="true">
+    <StyledCheckboxContainer {...props}>
+      <StyledCheckbox>
+        <StyledSvg viewBox="0 0 18 18" aria-hidden="true">
           <polyline points="1 9 7 14 15 4" />
-        </svg>
-      </div>
-      {props.label}
-    </StyledCheckbox>
+        </StyledSvg>
+      </StyledCheckbox>
+      <StyledLabel>{props.label}</StyledLabel>
+    </StyledCheckboxContainer>
   );
 };
 
---
+++ src/form/Switch/Switch.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import {
+  Switch as AriaSwitch,
+  SwitchProps as AriaSwitchProps,
+  Label,
+} from "react-aria-components";
+import styled from "styled-components";
+import { body1, sizing, theme } from "../../guidelines/theme";
+
+const StyledSwitch = styled(AriaSwitch)<{ isDisabled?: boolean }>`
+  &[data-disabled="true"] {
+    opacity: 0.5;
+    pointer-events: none;
+  }
+
+  display: flex;
+  align-items: center;
+  gap: ${sizing(10)};
+  ${body1}
+  forced-color-adjust: none;
+
+  .indicator {
+    width: ${sizing(32)};
+    height: ${sizing(18)};
+    border: 2px solid ${theme.color.gray};
+    background: ${theme.color.primary100};
+    border-radius: ${sizing(18)};
+    transition: all 200ms;
+
+    &:before {
+      content: "";
+      display: block;
+      margin: ${sizing(2)};
+      width: ${sizing(14)};
+      height: ${sizing(14)};
+      background: ${theme.color.primary};
+      border-radius: ${sizing(16)};
+      transition: all 200ms;
+    }
+  }
+
+  &[data-pressed] .indicator {
+    border-color: ${theme.color.gray};
+
+    &:before {
+      background: ${theme.color.primary100};
+    }
+  }
+
+  &[data-selected] {
+    .indicator {
+      border-color: ${theme.color.primary100};
+      background: ${theme.color.primary};
+
+      &:before {
+        background: ${theme.color.primary100};
+        transform: translateX(100%);
+      }
+    }
+
+    &[data-pressed] {
+      .indicator {
+        border-color: ${theme.color.gray200};
+        background: ${theme.color.primary};
+      }
+    }
+  }
+
+  &[data-focus-visible] .indicator {
+    outline: 2px solid ${theme.color.primary};
+    outline-offset: ${sizing(2)};
+  }
+`;
+
+const StyledLabel = styled(Label)`
+  ${body1};
+`;
+
+type SwitchProps = {
+  label?: string;
+} & Pick<
+  AriaSwitchProps,
+  | "inputRef"
+  | "defaultSelected"
+  | "isSelected"
+  | "value"
+  | "isDisabled"
+  | "name"
+  | "className"
+  | "autoFocus"
+>;
+
+const Switch = (props: SwitchProps) => {
+  return (
+    <StyledSwitch {...props}>
+      <div className="indicator" />
+      <StyledLabel>{props.label}</StyledLabel>
+    </StyledSwitch>
+  );
+};
+
+export default Switch;
