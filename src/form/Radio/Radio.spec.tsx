b119495dd1cc05e4119da239fdd6e0437c7324d7
Marc Potard <marc@marcpotard.com>
2024-06-26 10:42:59 +0200

    [Form] Fix useless import in Radio tests

--- src/form/Radio/Radio.spec.tsx
+++ src/form/Radio/Radio.spec.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import React from "react";
 import { describe, expect, it } from "vitest";
 import Radio from "../Radio/Radio";
