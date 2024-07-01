63f7f327db24a75f7ee3b23516d49b3709cfdbc7
Rcaradec <remi.caradec@matou-technologies.fr>
2024-07-01 17:49:52 +0200

    [Form] Upload - clean after rebase

--- src/action/Button/Button.tsx
+++ src/action/Button/Button.tsx
@@ -100,16 +100,6 @@ const buttonKind = (kind: Kind = "normal", variant: Variant = "primary") => {
   }
 };
 
-type ButtonProps = AriaButtonProps & {
-  kind?: Kind;
-  variant?: Variant;
-  size?: Size;
-  leadingIcon?: React.ReactNode;
-  trailingIcon?: React.ReactNode;
-  children?: React.ReactNode;
-  isPressed?: boolean;
-};
-
 const StyledButton = styled(AriaButton)<{
   kind?: Kind;
   variant?: Variant;
@@ -169,6 +159,5 @@ const Button = ({
   );
 };
 
-export type { ButtonProps };
 export default Button;
 export type { ButtonProps };
