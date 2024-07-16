c8437eee20728402a3b64b114230d796b8a86a6e
Rcaradec <remi.caradec@matou-technologies.fr>
2024-07-16 17:46:48 +0200

    [Purrfect-UI] Add typographies

--- src/index.ts
+++ src/index.ts
@@ -6,27 +6,51 @@ export type { DividerProps } from "src/display/Divider/Divider";
 
 export { default as Checkbox } from "src/form/Checkbox/Checkbox";
 export type { CheckboxProps } from "src/form/Checkbox/Checkbox";
+
 export { default as DatePicker } from "src/form/DatePicker/DatePicker";
 export type { DatePickerProps } from "src/form/DatePicker/DatePicker";
+
 export { default as Radio } from "src/form/Radio/Radio";
 export type { RadioProps } from "src/form/Radio/Radio";
+
 export { default as RadioGroup } from "src/form/RadioGroup/RadioGroup";
 export type { RadioGroupProps } from "src/form/RadioGroup/RadioGroup";
+
 export { default as Slider } from "src/form/Slider/Slider";
 export type { SliderProps } from "src/form/Slider/Slider";
+
 export { default as Switch } from "src/form/Switch/Switch";
 export type { SwitchProps } from "src/form/Switch/Switch";
+
 export { default as TextArea } from "src/form/TextArea/TextArea";
 export type { TextAreaProps } from "src/form/TextArea/TextArea";
+
 export { default as TextField } from "src/form/TextField/TextField";
 export type { TextFieldProps } from "src/form/TextField/TextField";
+
 export { default as Link } from "src/action/Link/Link";
 export type { LinkProps } from "src/action/Link/Link";
+
 export { default as Upload } from "src/form/Upload/Upload";
 export type { UploadProps } from "src/form/Upload/Upload";
+
 export { default as Tag } from "src/display/Tag/Tag";
 export type { TagProps } from "src/display/Tag/Tag";
+
 export { default as Table } from "src/display/Table/Table";
 export type { TableProps } from "src/display/Table/Table";
+
 export { default as TopBar } from "src/display/TopBar/TopBar";
 export type { TopBarProps } from "src/display/TopBar/TopBar";
+
+export {
+  heading1,
+  heading2,
+  heading3,
+  heading4,
+  heading5,
+  heading6,
+  body1,
+  body2,
+  narrow,
+} from "src/guidelines/theme";
