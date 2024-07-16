c7e4ec721b2cdff3d79fdbf5f67a3aa43655f4d6
Marc Potard <marc@marcpotard.com>
2024-07-16 12:07:37 +0200

    [Icons] Add sort and drag icons

---
+++ src/icons/Drag.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import IconSVG from "./IconSVG";
+
+const Drag = () => {
+  return (
+    <IconSVG
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 24 24"
+      height="200px"
+      width="200px"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path fill="none" d="M0 0h24v24H0V0z"></path>
+      <path d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
+    </IconSVG>
+  );
+};
+
+export default Drag;
