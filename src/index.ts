bc9bb418774a0a3cd73ecca205ce1b8b21ced496
Marc Potard <marc@marcpotard.com>
2024-07-26 11:38:39 +0200

    [Purrfect UI] Update index file

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,20 @@ export type { AvatarProps } from "src/display/Avatar/Avatar";
 export { default as Button } from "src/action/Button/Button";
 export type { ButtonProps } from "src/action/Button/Button";
 
-export { default as Divider } from "src/display/Divider/Divider";
-export type { DividerProps } from "src/display/Divider/Divider";
-
 export { default as Checkbox } from "src/form/Checkbox/Checkbox";
 export type { CheckboxProps } from "src/form/Checkbox/Checkbox";
 
 export { default as DatePicker } from "src/form/DatePicker/DatePicker";
 export type { DatePickerProps } from "src/form/DatePicker/DatePicker";
+
+export { default as Divider } from "src/display/Divider/Divider";
+export type { DividerProps } from "src/display/Divider/Divider";
+
+export { default as Link } from "src/action/Link/Link";
+export type { LinkProps } from "src/action/Link/Link";
+
+export { default as Loader } from "src/display/Loader/Loader";
+export type { LoaderProps } from "src/display/Loader/Loader";
 
 export { default as Radio } from "src/form/Radio/Radio";
 export type { RadioProps } from "src/form/Radio/Radio";
@@ -25,29 +31,23 @@ export type { SliderProps } from "src/form/Slider/Slider";
 export { default as Switch } from "src/form/Switch/Switch";
 export type { SwitchProps } from "src/form/Switch/Switch";
 
+export { default as Table } from "src/display/Table/Table";
+export type { TableProps } from "src/display/Table/Table";
+
+export { default as Tag } from "src/display/Tag/Tag";
+export type { TagProps } from "src/display/Tag/Tag";
+
+export { default as TopBar } from "src/display/TopBar/TopBar";
+export type { TopBarProps } from "src/display/TopBar/TopBar";
+
 export { default as TextArea } from "src/form/TextArea/TextArea";
 export type { TextAreaProps } from "src/form/TextArea/TextArea";
 
 export { default as TextField } from "src/form/TextField/TextField";
 export type { TextFieldProps } from "src/form/TextField/TextField";
 
-export { default as Link } from "src/action/Link/Link";
-export type { LinkProps } from "src/action/Link/Link";
-
-export { default as Loader } from "src/display/Loader/Loader";
-export type { LoaderProps } from "src/display/Loader/Loader";
-
 export { default as Upload } from "src/form/Upload/Upload";
 export type { UploadProps } from "src/form/Upload/Upload";
-
-export { default as Tag } from "src/display/Tag/Tag";
-export type { TagProps } from "src/display/Tag/Tag";
-
-export { default as Table } from "src/display/Table/Table";
-export type { TableProps } from "src/display/Table/Table";
-
-export { default as TopBar } from "src/display/TopBar/TopBar";
-export type { TopBarProps } from "src/display/TopBar/TopBar";
 
 export { default as Breadcrumb } from "src/navigation/Breadcrumb/Breadcrumb";
 export type { BreadcrumbProps } from "src/navigation/Breadcrumb/Breadcrumb";
