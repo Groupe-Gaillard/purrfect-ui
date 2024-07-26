1259954f1318b642405f856004b2bd5befa4b0c8
Cyril <cyril.j.cally+github@gmail.com>
2024-07-26 09:37:16 +0200

    [Purrfect-ui][Alert] init

---
+++ src/feedback/Alert/Alert.spec.tsx
@@ -0,0 +1,57 @@
+import {describe, expect} from "vitest";
+import Alert, {alertKindValues, alertSeverityValues} from "src/feedback/Alert/Alert";
+import {render, screen} from "@testing-library/react";
+import {theme} from "src/guidelines/theme";
+
+describe("Alert", () => {
+  it('Should render children', () => {
+    render(<Alert>Message</Alert>)
+
+    const alert = screen.getByText('Message')
+
+    expect(alert).toBeInTheDocument()
+  });
+
+  it('Should render with default props', () => {
+    render(<Alert>Message</Alert>)
+
+    const alert = screen.getByText('Message').parentElement as HTMLElement
+
+    expect(alert).toHaveStyle({
+      backgroundColor: theme.color.info,
+      color: theme.color.text.white,
+    })
+  })
+
+  alertKindValues.forEach((kind) => {
+    it(`Applies correct style for kind: ${kind}`, () => {
+      render(<Alert kind={kind}>Message</Alert>)
+
+      const alert = screen.getByText('Message').parentElement
+
+      if ('outlined' === kind) {
+        expect(alert).toHaveStyle({
+          backgroundColor: theme.color.white,
+          color: theme.color.info,
+          border: `2px solid ${theme.color.info}`
+        })
+      } else {
+        expect(alert).toHaveStyle({
+          backgroundColor: theme.color.info,
+        })
+      }
+    })
+  })
+
+  alertSeverityValues.forEach((severity) => {
+    it(`Applies correct style for severity: ${severity}`, () => {
+      render(<Alert severity={severity}>Message</Alert>)
+
+      const alert = screen.getByText('Message').parentElement
+
+      expect(alert).toHaveStyle({
+        backgroundColor: theme.color[severity],
+      })
+    })
+  })
+})
---
+++ src/feedback/Alert/Alert.stories.tsx
@@ -0,0 +1,72 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import styled from "styled-components";
+import Alert, { alertKindValues, alertSeverityValues } from "src/feedback/Alert/Alert";
+import { heading1 } from "src/guidelines/theme";
+import {isVowel, ucfirst} from "src/utils/stringHelper";
+
+const meta: Meta<typeof Alert> = {
+  component: Alert,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Alert>;
+
+const Title = styled.h1`
+  ${heading1};
+`;
+
+const FlexContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const AlertDemo: Story = {
+  args: {
+    severity: "info",
+    kind: "normal",
+    children: "Message",
+  },
+  argTypes: {
+    severity: {
+      options: alertSeverityValues,
+      mapping: { alertSeverityValues },
+      control: { type: "select" },
+      description: "",
+    },
+    kind: {
+      options: alertKindValues,
+      mapping: { alertKindValues },
+      control: { type: "select" },
+      description: "",
+    }
+  },
+};
+
+export const AlertList = () => {
+  return (
+    <FlexContainer>
+      {
+        alertKindValues.map(
+          (kind) => (
+            <>
+              <Title>{ucfirst(kind)}</Title>
+              <FlexContainer>
+                {
+                  alertSeverityValues.map(
+                    (severity) => (
+                      <Alert severity={severity} kind={kind} key={`${severity}-${kind}`}>
+                        {`This ${isVowel(severity.charAt(0)) ? 'an' : 'a'} ${severity} message`}
+                      </Alert>
+                    )
+                  )
+                }
+              </FlexContainer>
+            </>
+          )
+        )
+      }
+    </FlexContainer>
+  )
+}
